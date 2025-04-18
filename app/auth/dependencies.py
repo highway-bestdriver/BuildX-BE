f2b5a49323705fbe69ef45e72ee6f01002c67bc6
Hwang-Jaeryeong <jryeong67@naver.com>
2025-04-18 16:22:07 +0900

    fix: auth 조건 수정

--- app/auth/dependencies.py
+++ app/auth/dependencies.py
@@ -10,9 +10,16 @@ from fastapi.security import OAuth2
 from fastapi.openapi.models import OAuthFlows as OAuthFlowsModel, OAuthFlowPassword
 
 # OAuth2PasswordBearer: JWT 토큰을 가져오기 위한 FastAPI 내장 기능
-oauth2_scheme = OAuth2PasswordBearer(tokenUrl="auth/login")
+# oauth2_scheme = OAuth2PasswordBearer(tokenUrl="auth/login")
 
-def get_current_user(token: str = Depends(oauth2_scheme), db: Session = Depends(get_db)):
+class OAuth2PasswordBearerNoClient(OAuth2):
+    def __init__(self, tokenUrl: str):
+        flows = OAuthFlowsModel(password=OAuthFlowPassword(tokenUrl=tokenUrl))
+        super().__init__(flows=flows)
+
+oauth2_scheme_no_client = OAuth2PasswordBearerNoClient(tokenUrl="auth/login")
+
+def get_current_user(token: str = Depends(oauth2_scheme_no_client), db: Session = Depends(get_db)):
     """ JWT 토큰에서 현재 로그인한 유저 정보를 가져오는 함수 """
     try:
         payload = jwt.decode(token, SECRET_KEY, algorithms=[ALGORITHM])
@@ -27,10 +34,3 @@ def get_current_user(token: str = Depends(oauth2_scheme), db: Session = Depends(
         raise HTTPException(status_code=401, detail="사용자를 찾을 수 없습니다.")
 
     return user
-
-class OAuth2PasswordBearerNoClient(OAuth2):
-    def __init__(self, tokenUrl: str):
-        flows = OAuthFlowsModel(password=OAuthFlowPassword(tokenUrl=tokenUrl))
-        super().__init__(flows=flows)
-
-oauth2_scheme_no_client = OAuth2PasswordBearerNoClient(tokenUrl="auth/login")
